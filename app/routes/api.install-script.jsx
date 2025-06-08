396f85c762f99e4caf00b870bed7ad46eb4d067e
serkanakyol <serkan.akyol@venhancer.com>
2025-06-08 20:58:55 +0200

    Modified Dashbaord

--- app/routes/api.install-script.jsx
+++ app/routes/api.install-script.jsx
@@ -1,6 +1,7 @@
 import { authenticate } from "../shopify.server";
 
 export async function action({ request }) {
+
   const { admin } = await authenticate.admin(request);
 
   const scriptTag = {
@@ -12,33 +13,15 @@ export async function action({ request }) {
   };
 
   try {
-    const response = await admin.post({
-      path: "script_tags",
-      data: scriptTag,
-      type: "application/json",
-    });
+    console.log("ScriptTag resource:", admin.rest);
+    console.log("ScriptTag resource:", admin.rest.resources.ScriptTag);
 
     return new Response(JSON.stringify({ success: true, id: response?.body?.script_tag?.id }), {
       status: 200,
     });
+
   } catch (err) {
     console.error("ScriptTag API Error", err);
     return new Response(JSON.stringify({ success: false }), { status: 500 });
   }
-/*
-  try {
-    console.log("ScriptTag resource:", admin.rest);
-    console.log("ScriptTag resource:", admin.rest.resources.ScriptTag);
-    const response = await admin.rest.resources.ScriptTag({
-      body: scriptTag,
-    });
-console.log(response)
-    return new Response(JSON.stringify({ success: true, id: response.id }), {
-      status: 200,
-    });
-  } catch (err) {
-    console.error("ScriptTag API Error", err);
-    return new Response(JSON.stringify({ success: false }), { status: 500 });
-  }
-*/
 }

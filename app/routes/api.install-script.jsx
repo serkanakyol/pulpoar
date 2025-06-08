3b00b62583662e2274181457ff9a4885506742de
serkanakyol <serkan.akyol@venhancer.com>
2025-06-08 20:53:39 +0200

    Modified Dashbaord

--- app/routes/api.install-script.jsx
+++ app/routes/api.install-script.jsx
@@ -1,19 +1,7 @@
-import { shopifyApi } from "@shopify/shopify-api";
-import { restResources } from "@shopify/shopify-api/rest/admin/2025-01";
 import { authenticate } from "../shopify.server";
 
-const shopify = shopifyApi({
-  apiKey: process.env.SHOPIFY_API_KEY || "",
-  apiSecretKey: process.env.SHOPIFY_API_SECRET || "",
-  apiVersion: "2025-01",
-  restResources,
-});
-
 export async function action({ request }) {
-  const { session } = await authenticate.admin(request);
-  const admin = new shopify.clients.Rest({
-    session,
-  });
+  const { admin } = await authenticate.admin(request);
 
   const scriptTag = {
     script_tag: {
@@ -23,14 +11,13 @@ export async function action({ request }) {
     },
   };
 
-
   try {
     const response = await admin.post({
       path: "script_tags",
       data: scriptTag,
       type: "application/json",
     });
-console.log(response);
+
     return new Response(JSON.stringify({ success: true, id: response?.body?.script_tag?.id }), {
       status: 200,
     });
@@ -38,4 +25,20 @@ console.log(response);
     console.error("ScriptTag API Error", err);
     return new Response(JSON.stringify({ success: false }), { status: 500 });
   }
+/*
+  try {
+    console.log("ScriptTag resource:", admin.rest);
+    console.log("ScriptTag resource:", admin.rest.resources.ScriptTag);
+    const response = await admin.rest.resources.ScriptTag({
+      body: scriptTag,
+    });
+console.log(response)
+    return new Response(JSON.stringify({ success: true, id: response.id }), {
+      status: 200,
+    });
+  } catch (err) {
+    console.error("ScriptTag API Error", err);
+    return new Response(JSON.stringify({ success: false }), { status: 500 });
+  }
+*/
 }

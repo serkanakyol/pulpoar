60188385671c34f958acee7bee6eaee21e5af3b4
serkanakyol <serkan.akyol@venhancer.com>
2025-06-08 23:47:40 +0200

    Modified Dashbaord

--- app/utils/pulpoar.js
+++ app/utils/pulpoar.js
@@ -1,7 +1,8 @@
-export function buildPulpoarScriptUrl()
-{
+export function buildPulpoarScriptUrl() {
   const baseUrl = process.env.PULPOAR_SCRIPT_BASE_URL || "";
   const params = new URLSearchParams();
-  return `${baseUrl}?${params.toString()}`;
+
+  const queryString = params.toString();
+  return queryString ? `${baseUrl}?${queryString}` : baseUrl;
   //return `${baseUrl}?shop=${encodeURIComponent(shopDomain)}`;
 }

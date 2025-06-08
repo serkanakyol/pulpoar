8b83fb1d2e754399c98648e5c4dbebe40265126d
serkanakyol <serkan.akyol@venhancer.com>
2025-06-09 01:58:25 +0200

    Modified Dashbaord

--- app/routes/api.product.jsx
+++ app/routes/api.product.jsx
@@ -2,7 +2,12 @@ import { json } from "@remix-run/node";
 import { authenticate } from "../shopify.server";
 
 export const loader = async ({ request }) => {
-  const { admin } = await authenticate.admin(request);
+  const { admin, session } = await authenticate.admin(request);
+
+  if (!session?.shop) {
+    throw new Error("Shop information missing in session.");
+  }
+
   const response = await admin.rest.get("/admin/api/2024-01/products.json");
 
   const products = response.body.products.map((product) => ({

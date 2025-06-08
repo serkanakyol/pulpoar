dbe44771d94f6988b2284a2d3eb508f0c78035a3
serkanakyol <serkan.akyol@venhancer.com>
2025-06-08 23:34:43 +0200

    Modified Dashbaord

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,7 @@
 import { flatRoutes } from "@remix-run/fs-routes";
 
 export default flatRoutes((route) => {
-  route("/", "routes/index.jsx");
-  route("/api/install-script", "routes/app/api/install-script.jsx");
-    route("/api/remove-script", "routes/app/api/remove-script.jsx");
+    route("/", "routes/index.jsx");
+    route("/api/install-script", "routes/api/install-script.jsx");
+    route("/api/remove-script", "routes/api/remove-script.jsx");
 });

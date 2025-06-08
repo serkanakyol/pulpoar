664b9d09a8c9a97084027209cac89f8913035fcd
serkanakyol <serkan.akyol@venhancer.com>
2025-06-08 21:57:49 +0200

    Modified Dashbaord

---
+++ app/utils/pulpoar.js
@@ -0,0 +1,9 @@
+function buildPulpoarScriptUrl({ shop, userId }) {
+  const baseUrl = process.env.PULPOAR_SCRIPT_BASE_URL;
+  const params = new URLSearchParams();
+
+  //if (shop) params.append("shop", shop);
+  //if (userId) params.append("user_id", userId);
+
+  return `${baseUrl}?${params.toString()}`;
+}

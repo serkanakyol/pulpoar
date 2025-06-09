b54402af3ebb583353d8881221628da1628495ba
serkanakyol <serkan.akyol@venhancer.com>
2025-06-09 02:05:30 +0200

    Modified Dashbaord

--- app/routes/api.product.jsx
+++ app/routes/api.product.jsx
@@ -1,18 +1,50 @@
 import { json } from "@remix-run/node";
-import { authenticate } from "../shopify.server";
+import { authenticate, unauthenticated } from "../shopify.server";
 
 export const loader = async ({ request }) => {
   const { admin, session } = await authenticate.admin(request);
 
   if (!session?.shop) {
-    throw new Error("Shop information missing in session.");
+       return unauthenticated.redirectToAuth({ request });
   }
+ const query = `
+    {
+      products(first: 50) {
+        edges {
+          node {
+            id
+            title
+            handle
+            featuredImage {
+              url
+              altText
+            }
+            variants(first: 1) {
+              edges {
+                node {
+                  id
+                  price {
+                    amount
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
 
-  const response = await admin.rest.get("/admin/api/2024-01/products.json");
+  const gqlResponse = await admin.graphql(query);
+  const jsonResponse = await gqlResponse.json();
 
-  const products = response.body.products.map((product) => ({
-    id: product.id,
-    title: product.title,
+  const products = jsonResponse.data.products.edges.map(({ node }) => ({
+    id: node.id,
+    title: node.title,
+    handle: node.handle,
+    image: node.featuredImage?.url || null,
+    variantId: node.variants.edges[0]?.node.id || null,
+    price: node.variants.edges[0]?.node.price.amount || null,
   }));
 
   return json(products);

292b0f6fdaf402fd939edcf97cb128c94ad6a6d8
serkanakyol <serkan.akyol@venhancer.com>
2025-06-09 12:52:08 +0200

    Modified Dashbaord

--- vite.config.js
+++ vite.config.js
@@ -68,6 +68,6 @@ export default defineConfig({
     assetsInlineLimit: 0,
   },
   optimizeDeps: {
-    include: ["@shopify/app-bridge-react", "@shopify/polaris"],
+    include: ["@shopify/app-bridge", "@shopify/app-bridge-react", "@shopify/app-bridge/actions", "@shopify/polaris"],
   }
 });

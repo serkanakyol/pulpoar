9702592ba1a4dca81276c91c013251936bf95a5b
serkanakyol <serkan.akyol@venhancer.com>
2025-06-09 17:15:09 +0200

    Modified Dashbaord

--- app/root.jsx
+++ app/root.jsx
@@ -4,53 +4,28 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-  useLoaderData,
+  useLoaderData
 } from "@remix-run/react";
-import { json } from "@remix-run/node";
-import { useEffect, useState } from "react";
-import createApp from "@shopify/app-bridge";
-
-export async function loader({ request }) {
-  const url = new URL(request.url);
-  const host = url.searchParams.get("host");
-
-  return json({
-    host,
-    apiKey: process.env.SHOPIFY_API_KEY,
-  });
-}
 
 export default function App() {
-  const { host, apiKey } = useLoaderData();
-  const [appBridge, setAppBridge] = useState(null);
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const app = createApp({
-        apiKey,
-        host,
-        forceRedirect: true,
-      });
-      setAppBridge(app);
-    }
-  }, [host, apiKey]);
 
   return (
     <html>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link rel="preconnect" href="https://cdn.shopify.com/" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.shopify.com/static/fonts/inter/v4/styles.css"
+        />
         <Meta />
         <Links />
       </head>
       <body>
-        {appBridge ? (
-          <Outlet context={{ appBridge }} />
-        ) : (
-          <div>Yükleniyor...</div>
-        )}
-        <ScrollRestoration />
-        <Scripts />
+          <Outlet />
+          <ScrollRestoration />
+          <Scripts />
       </body>
     </html>
   );
